19192d9c31f8430626fe87cb7b4d971536e78e38
Jan Nahody <jan.nahody@gmail.com>
2017-07-18 21:57:35 +0200

    Move NavigationBar component to own file

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  Link
+} from 'react-router-dom';
+import './navigation-bar.css';
+
+const NavigationBar = () => (
+    <div className="navigation-bar__wrapper">
+        <div className="navigation-bar__placeholder"></div>
+        <nav className="navigation-bar">
+            <Link className="navigation-bar__item--flex" to="/">Home</Link>
+            <Link className="navigation-bar__item" to="/login">login</Link>
+        </nav>
+    </div>
+)
+
+export { NavigationBar }
