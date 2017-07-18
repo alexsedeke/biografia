353855d8ce9b853e5acbdeab3a20f4ebb740cb64
Jan Nahody <jan.nahody@gmail.com>
2017-07-18 21:58:01 +0200

    Create universal import

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,17 +1,1 @@
-import React from 'react'
-import {
-  Link
-} from 'react-router-dom';
-import './navigation-bar.css';
-
-const NavigationBar = () => (
-    <div className="navigation-bar__wrapper">
-        <div className="navigation-bar__placeholder"></div>
-        <nav className="navigation-bar">
-            <Link className="navigation-bar__item--flex" to="/">Home</Link>
-            <Link className="navigation-bar__item" to="/login">login</Link>
-        </nav>
-    </div>    
-)
-
-export { NavigationBar }
+export { NavigationBar } from './NavigationBar'
