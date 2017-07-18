744622564cb9e016a35ac9792d1ad11a65b22d7a
Jan Nahody <jan.nahody@gmail.com>
2017-07-18 16:16:35 +0200

    Add styling to Navigation Bar

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import {
   Link
-} from 'react-router-dom'
+} from 'react-router-dom';
+import './navigation-bar.css';
 
 const NavigationBar = () => (
-  <nav className="navigation-bar">
-    <Link className="navigation-bar__item" to="/">Home</Link>
-    <Link className="navigation-bar__item" to="/login">login</Link>
-  </nav>
+    <div className="navigation-bar__wrapper">
+        <div className="navigation-bar__placeholder"></div>
+        <nav className="navigation-bar">
+            <Link className="navigation-bar__item--flex" to="/">Home</Link>
+            <Link className="navigation-bar__item" to="/login">login</Link>
+        </nav>
+    </div>    
 )
 
 export { NavigationBar }
