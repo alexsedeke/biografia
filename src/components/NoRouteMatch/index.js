42ffdf6cc39db97b293c3160dc09bf5100ae2e55
Jan Nahody <jan.nahody@gmail.com>
2017-07-18 15:43:41 +0200

    Add noRouteMatch component

---
+++ src/components/NoRouteMatch/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoRouteMatch = () => (
+    <div className="page-noMatch">
+        <h2>Sorry but we do have your are looking for.</h2>
+    </div>
+)
+
+export { NoRouteMatch };
