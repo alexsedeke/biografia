8b91a9e5a4fba03695740021b34d377ed01c4150
Jan Nahody <jan.nahody@gmail.com>
2017-07-19 21:57:16 +0200

    Preparing login page

--- src/components/PageLogin/index.js
+++ src/components/PageLogin/index.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PageLogin = () => (
-    <div className="page-home">
-        <h2>so you are now on page login</h2>
-    </div>
-)
+class PageLogin extends React.Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    setUsername = (evt) => {
+        this.setState({
+            username: evt.target.value
+        });
+    }
+
+    setPassword = (evt) => {
+        this.setState({
+            password: evt.target.value
+        });
+    }
+
+    handleLogin() {
+        // handle login here
+    }
+
+    render() {
+        return (
+            <div className="page-home">
+                <h1>login</h1>
+                <form onSubmit={this.handleLogin}>
+                    <label>
+                        Username:
+                        <input type="text" value={this.state.username} onChange={this.setUsername} placeholder="Your user name" autoFocus required />
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" value={this.state.password} onChange={this.setPassword} placeholder="Your secret password" required />
+                    </label>
+                    <button>Login</button>
+                </form>
+            </div>
+        )
+    }
+}
 
 export { PageLogin };
