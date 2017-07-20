75f71d551861833337efb6768895bb8b0fee3d61
Jan Nahody <jan.nahody@gmail.com>
2017-07-20 11:55:48 +0200

    Add config file

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default const config = {
+    firebase: {
+        apiKey: "AIzaSyCn-TMQ0YOBGDVUwYwFDWKhXzlu1eLz8I8",
+        authDomain: "biografia-20edc.firebaseapp.com",
+        databaseURL: "https://biografia-20edc.firebaseio.com",
+        projectId: "biografia-20edc",
+        storageBucket: "biografia-20edc.appspot.com",
+        messagingSenderId: "484938172794"
+    }
+}
