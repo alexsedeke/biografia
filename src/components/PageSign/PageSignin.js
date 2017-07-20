0767442a4eb3e93e5de63e4f7baf100c306c5300
Jan Nahody <jan.nahody@gmail.com>
2017-07-20 22:11:40 +0200

    Rename PageLogin to PageSignin

---
+++ src/components/PageSign/PageSignin.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import firebase from 'firebase';
+//import PropTypes from 'prop-types';
+
+class PageSignin extends React.Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    setUsername = (evt) => {
+        this.setState({
+            username: evt.target.value
+        });
+    }
+
+    setPassword = (evt) => {
+        this.setState({
+            password: evt.target.value
+        });
+    }
+
+    handleLogin = (evt) => {
+        evt.preventDefault();
+        // handle login here
+        const auth = firebase.auth();
+        const signinPromise = auth.signInWithEmailAndPassword( this.state.username, this.state.password );
+        signinPromise.then(() => {
+            console.log('done');
+        }).catch(e => {
+            console.log(e.message)
+        });
+    }
+
+
+    render() {
+        return (
+            <div className="page-home">
+                <h1>login</h1>
+                <form onSubmit={this.handleLogin}>
+                    <label>
+                        Username:
+                        <input type="email" value={this.state.username} onChange={this.setUsername} placeholder="Your user name" autoFocus required />
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" value={this.state.password} onChange={this.setPassword} placeholder="Your secret password" required />
+                    </label>
+                    <button>Login</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export { PageSignin };
