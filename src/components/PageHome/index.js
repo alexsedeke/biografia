7097503e1ddb522bb9182eaceebae32a1398a16c
Jan Nahody <jan.nahody@gmail.com>
2017-07-20 12:45:56 +0200

    First read from database

--- src/components/PageHome/index.js
+++ src/components/PageHome/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import firebase from 'firebase';
 
-const PageHome = () => (
-    <div className="page-home">
-        <h2>so you are now on page home</h2>
-    </div>
-)
+class PageHome extends React.Component {
+    state = {
+        dbval: 'static'
+    }
+
+    componentDidMount() {
+        const cvRef = firebase.database().ref().child('Config');
+        cvRef.on('value', snap => {
+            this.setState({
+                dbval: snap.val()
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className="page-home">
+                <h2>so you are now on page home {this.state.dbval}</h2>
+            </div>
+        )
+    }
+}
 
 export { PageHome };
