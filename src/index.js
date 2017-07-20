8b499278c64d4121f8c0c4ecde93fc9efe2b5e58
Jan Nahody <jan.nahody@gmail.com>
2017-07-20 22:15:32 +0200

    Update firebase import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import firebase from 'firebase';
+import * as firebase from 'firebase';
 import config from './config';
 
 firebase.initializeApp( config.firebase );
