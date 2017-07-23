12d3d914190ab01a9f7f90f2100bc20c8f66a414
Jan Nahody <jan.nahody@gmail.com>
2017-07-23 13:32:44 +0200

    Update authentication verification of NavigationBar Component

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -3,15 +3,51 @@ import {
   Link
 } from 'react-router-dom';
 import './navigation-bar.css';
+import { isAuthenticated } from '../../helper/auth';
 
-const NavigationBar = () => (
-    <div className="navigation-bar__wrapper">
-        <div className="navigation-bar__placeholder"></div>
-        <nav className="navigation-bar">
-            <Link className="navigation-bar__item--flex" to="/">Home</Link>
-            <Link className="navigation-bar__item" to="/signin">Sign In</Link>
-        </nav>
-    </div>
-)
+class NavigationBar extends React.Component {
+
+    authenticated() {
+        return (
+            <div className="navigation-bar__wrapper">
+               <div className="navigation-bar__placeholder"></div>
+               <nav className="navigation-bar">
+                    <div className="navigation-bar__item--flex">
+                        <Link className="navigation-bar__link" to="/">Home</Link>
+                    </div>
+                    <div className="navigation-bar__item">
+                        <Link className="navigation-bar__link" to="/admin">Admin</Link>
+                        <Link className="navigation-bar__link" to="/signout">Sign Out</Link>
+                    </div>
+               </nav>
+           </div>
+        )
+    }
+
+    unauthenticated() {
+        return (
+            <div className="navigation-bar__wrapper">
+               <div className="navigation-bar__placeholder"></div>
+               <nav className="navigation-bar">
+                    <div className="navigation-bar__item--flex">
+                        <Link className="navigation-bar__link" to="/">Home</Link>
+                    </div>
+                    <div className="navigation-bar__item">
+                        <Link className="navigation-bar__link" to="/signin">Sign In</Link>
+                    </div>
+               </nav>
+           </div>
+        )
+    }
+
+    render() {
+        if (isAuthenticated()) {
+            return this.authenticated();
+        } else {
+            return this.unauthenticated();
+        }
+
+    }
+}
 
 export { NavigationBar }
