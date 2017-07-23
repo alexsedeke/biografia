27ada7a81227e5e1d466796a360c8a404eec2891
Jan Nahody <jan.nahody@gmail.com>
2017-07-23 13:30:25 +0200

    Update SecuredRoute component to avoid firebase lazy user fetching

--- src/components/PageRouter/SecuredRoute.js
+++ src/components/PageRouter/SecuredRoute.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import * as firebase from 'firebase';
+import { isAuthenticated } from '../../helper/auth';
 
-class SecuredRoute extends React.Component {
-    render() {
-        const { component: Component, ...rest } = this.props;
-        const auth = firebase.auth().currentUser;
-
-        return (
-            auth
-            ?<Redirect to="/signin" />
-            :<Route {...rest} render={ props => <Component {...props} /> } />
-        )
-    }
-}
-
-export { SecuredRoute };
+export const SecuredRoute = ({ component: Component, ...rest }) => (
+    isAuthenticated()
+    ?<Route {...rest} render={ props => <Component {...props} /> } />
+    :<Redirect to="/signin" />
+);
