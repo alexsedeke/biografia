efb3aae785e1842d461613abccf1207dcc917ebb
Jan Nahody <jan.nahody@gmail.com>
2017-07-23 13:28:54 +0200

    Add user to localStorage

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ import React, {
 //import logo from './logo.svg';
 import './App.css';
 import { PageRouter } from './components/PageRouter';
+import * as firebase from 'firebase';
 
 class App extends Component {
+    state = {
+        currentUser: null
+    }
+
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged(  user => {
+            if (user === null) {
+                localStorage.removeItem('user', null);
+            } else {
+                localStorage.setItem('user', user.uid);
+            }
+
+            this.setState({
+                currentUser: user
+            });
+
+        } );
+    }
+
     render() {
         return(
             <div className="App">
