6b07ce7bc015845faa54f30e9acbc4ed30fe255d
Jan Nahody <jan.nahody@gmail.com>
2017-07-23 13:35:25 +0200

    Create unique export file fro AdminPage section

--- src/components/PageAdmin/index.js
+++ src/components/PageAdmin/index.js
@@ -1,27 +1,2 @@
-import React from 'react';
-import firebase from 'firebase';
-
-class PageAdmin extends React.Component {
-    state = {
-        dbval: 'Admin'
-    }
-
-    componentDidMount() {
-        // const cvRef = firebase.database().ref().child('Config');
-        // cvRef.on('value', snap => {
-        //     this.setState({
-        //         dbval: snap.val()
-        //     });
-        // });
-    }
-
-    render() {
-        return (
-            <div className="page-admin">
-                <h2>so you are now on page admin {this.state.dbval}</h2>
-            </div>
-        )
-    }
-}
-
-export { PageAdmin };
+export { AdminRouter } from './Router';
+export { Timeline } from './Timeline';
