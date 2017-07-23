b47f1aeeef9ac1e0c4577d5478633999cd463e0d
Jan Nahody <jan.nahody@gmail.com>
2017-07-23 13:28:19 +0200

    Create router fro admin section

---
+++ src/components/PageAdmin/Router.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  Link
+} from 'react-router-dom';
+import { Profile } from './Profile';
+import { Timeline } from './Timeline';
+
+class AdminRouter extends React.Component {
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Link to="/admin/profile">Profile</Link>
+                    <Link to="/admin/timeline">Timeline</Link>
+
+                    <Route path="/admin" render={() => (
+                        <Redirect to="/admin/profile" />
+                    )} />
+                    <Route path="/admin/profile" component={Profile} />
+                    <Route path="/admin/timeline" component={Timeline} />
+                </div>
+            </Router>
+        )
+    }
+}
+
+export { AdminRouter };
