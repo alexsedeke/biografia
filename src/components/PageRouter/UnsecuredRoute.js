318d8beacc6903599d0775845197720ca916f94f
Jan Nahody <jan.nahody@gmail.com>
2017-07-23 13:30:37 +0200

    Update UnsecuredRoute component to avoid firebase lazy user fetching

--- src/components/PageRouter/UnsecuredRoute.js
+++ src/components/PageRouter/UnsecuredRoute.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import * as firebase from 'firebase';
+import { isAuthenticated } from '../../helper/auth';
 
-class UnsecuredRoute extends React.Component{
-    render() {
-        const { component: Component, ...rest } = this.props;
-        const auth = firebase.auth().currentUser;
-
-        return (
-            auth
-            ?<Redirect to="/" />
-            :<Route {...rest} render={ props => <Component {...props} /> } />
-        )
-    }
-}
-
-export { UnsecuredRoute };
+export const UnsecuredRoute = ({ component: Component, ...rest }) => (
+    isAuthenticated()
+    ?<Redirect to="/" />
+    :<Route {...rest} render={ props => <Component {...props} /> } />
+);
