efaf55ffb82d2023c3d21ab1a838c4e3408adadb
Jan Nahody <jan.nahody@gmail.com>
2017-07-24 21:02:53 +0200

    Add postcss-nested plugin to config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = (ctx) => ({
     plugins: {
         'postcss-import': { root: ctx.file.dirname },
+        'postcss-nested': {},
         'postcss-cssnext': {},
         'postcss-discard-comments': { removeAll: true },
         'cssnano': ctx.env === 'production' ? {} : false
