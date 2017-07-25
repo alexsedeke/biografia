3c0ca3bac1f73b37f37e949573954f0c81c5d2cf
Jan Nahody <jan.nahody@gmail.com>
2017-07-25 11:48:44 +0200

    Update Admin subnavigation structure

--- src/components/Admin/Menubar.js
+++ src/components/Admin/Menubar.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Menubar = () => (
-    <div className="menu-bar">
-        <Link to="/admin/profile">Profile</Link>
-        <Link to="/admin/socials">Socials</Link>
-        <Link to="/admin/sections">Sections</Link>
+    <div className="sub-navigation">
+        <div className="limit-width">
+            <Link className="sub-navigation__link" to="/admin/profile"><span>Profile</span></Link>
+            <Link className="sub-navigation__link" to="/admin/socials"><span>Socials</span></Link>
+            <Link className="sub-navigation__link" to="/admin/sections"><span>Sections</span></Link>
+        </div>
     </div>
 );
