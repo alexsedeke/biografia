b2bc1929cdd79e5fede1d9639acfcca8d01d67d4
Jan Nahody <jan.nahody@gmail.com>
2017-07-25 13:24:54 +0200

    Update Admin Profile component placeholder

--- src/components/Admin/Profile.js
+++ src/components/Admin/Profile.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Profile extends React.Component {
     render() {
         return (
-            <div>Profile</div>
+            <div className="profile-view limit-width">
+                <h2>Profile placeholder</h2>
+            </div>
         );
     }
 }
