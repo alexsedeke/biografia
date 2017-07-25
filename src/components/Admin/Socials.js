063798bdbd40215b0161ce8161eca70c1d196866
Jan Nahody <jan.nahody@gmail.com>
2017-07-25 13:24:38 +0200

    Update Admin Socials component placeholder

--- src/components/Admin/Socials.js
+++ src/components/Admin/Socials.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Socials extends React.Component {
     render() {
         return (
-            <div>Socials</div>
+            <div className="socials-view limit-width">
+                <h2>Socials placeholder</h2>
+            </div>
         );
     }
 }
