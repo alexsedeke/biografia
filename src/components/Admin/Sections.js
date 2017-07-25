f814302f83fa81ed9f620fa7f0fa4daf20b72dd1
Jan Nahody <jan.nahody@gmail.com>
2017-07-25 11:11:51 +0200

    Create Socials Component for Admin

---
+++ src/components/Admin/Sections.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Sections extends React.Component {
+    render() {
+        return (
+            <div>Sections</div>
+        );
+    }
+}
+
+export { Sections };
