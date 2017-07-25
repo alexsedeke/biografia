23b2b38577c72035c94bc5d2c22dc1b61efb55cc
Jan Nahody <jan.nahody@gmail.com>
2017-07-25 13:24:47 +0200

    Update Admin Sections component placeholder

--- src/components/Admin/Sections.js
+++ src/components/Admin/Sections.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Sections extends React.Component {
     render() {
         return (
-            <div>Sections</div>
+            <div className="sections-view limit-width">
+                <h2>Sections placeholder</h2>
+            </div>
         );
     }
 }
