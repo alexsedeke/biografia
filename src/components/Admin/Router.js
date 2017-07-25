4d4c10a2413d0030ee6b3a2d8c5f41ed0863ec2c
Jan Nahody <jan.nahody@gmail.com>
2017-07-25 11:12:32 +0200

    Update Admin Router with new components

--- src/components/Admin/Router.js
+++ src/components/Admin/Router.js
@@ -5,23 +5,24 @@ import {
   Redirect,
   Link
 } from 'react-router-dom';
+import { Menubar } from './Menubar';
 import { Profile } from './Profile';
-import { Timeline } from './Timeline';
+import { Socials } from './Socials';
+import { Sections } from './Sections';
 
 class AdminRouter extends React.Component {
 
     render() {
         return (
             <Router>
-                <div>
-                    <Link to="/admin/profile">Profile</Link>
-                    <Link to="/admin/timeline">Timeline</Link>
-
+                <div className="admin-view">
+                    <Menubar />
                     <Route path="/admin" render={() => (
                         <Redirect to="/admin/profile" />
                     )} />
                     <Route path="/admin/profile" component={Profile} />
-                    <Route path="/admin/timeline" component={Timeline} />
+                    <Route path="/admin/socials" component={Socials} />
+                    <Route path="/admin/sections" component={Sections} />
                 </div>
             </Router>
         )
