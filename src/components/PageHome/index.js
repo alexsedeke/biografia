fd7429213b8eca4964235eedfcc2197cf2c7568c
Jan Nahody <jan.nahody@gmail.com>
2017-07-25 13:19:02 +0200

    Udpate Home Component as placeholder

--- src/components/PageHome/index.js
+++ src/components/PageHome/index.js
@@ -2,23 +2,14 @@ import React from 'react';
 import firebase from 'firebase';
 
 class PageHome extends React.Component {
-    state = {
-        dbval: 'static'
-    }
-
-    componentDidMount() {
-        const cvRef = firebase.database().ref().child('Config');
-        cvRef.on('value', snap => {
-            this.setState({
-                dbval: snap.val()
-            });
-        });
-    }
-
     render() {
+        let tmpStyle = {
+            textAlign: 'center'
+        };
+
         return (
-            <div className="page-home">
-                <h2>so you are now on page home {this.state.dbval}</h2>
+            <div className="home-view limit-width">
+                <h2 style={tmpStyle}>Home view under construction</h2>
             </div>
         )
     }
