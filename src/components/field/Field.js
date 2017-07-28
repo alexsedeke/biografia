6d51170b4de8aa4a6dcbddb428f3a00e2a9f6b4e
Jan Nahody <jan.nahody@gmail.com>
2017-07-28 12:42:13 +0200

    Create Field Structure

--- src/components/field/Field.js
+++ src/components/field/Field.js
@@ -1,11 +1,30 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 class Field extends React.Component {
-
     render() {
-        return ();
+        return (
+            <div className="form__subset">
+                {this.props.label
+                    ?<label htmlFor="firstname" className="field__label">{this.props.label}</label>
+                    :null}
+                <div className="field__wrapper">
+                    <input type={this.props.type} id={this.props.id} value={this.props.value} onChange={this.props.handleUpdate} onBlur={this.props.handleBlur} placeholder={this.props.placeholder} />
+                </div>
+            </div>
+        );
     }
+}
+
+Field.propTypes = {
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    label: PropTypes.string,
+    required: PropTypes.string,
+    handleUpdate: PropTypes.func.isRequired,
+    handleBlur:PropTypes.func
 }
 
 export { Field };
