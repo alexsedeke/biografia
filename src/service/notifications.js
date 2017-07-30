52a871b65bcfa0187eb70268864cb8e7c73ff67d
Jan Nahody <jan.nahody@gmail.com>
2017-07-30 17:53:19 +0200

    Create notification service

---
+++ src/service/notifications.js
@@ -0,0 +1,25 @@
+const EventEmitter = require( "events" );
+
+/*
+ * Simple Notifivation Service
+ */
+export class Notifications extends EventEmitter {
+
+    constructor() {
+        super();
+        this.messages = [];
+    }
+
+    add( message, type = 'info') {
+        let messageObject = {
+            type: type,
+            message: message,
+            timestamp: new Date()
+        }
+
+        this.messages.push( messageObject );
+        this.emit('notification', messageObject);
+    }
+}
+
+export let notifications = new Notifications();
