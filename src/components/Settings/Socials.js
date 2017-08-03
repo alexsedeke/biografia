66251327789d53fcbfd373eea9d634c6387124e6
Jan Nahody <jan.nahody@gmail.com>
2017-08-03 12:12:52 +0200

    Add adding new social record

--- src/components/Settings/Socials.js
+++ src/components/Settings/Socials.js
@@ -7,7 +7,7 @@ import './socials.css';
 
 class Socials extends React.Component {
     state = {
-
+        socials: {}
     }
 
     constructor() {
@@ -15,10 +15,40 @@ class Socials extends React.Component {
         this.db = firebase.database().ref();
     }
 
+    componentDidMount() {
+        this.db.child('socials').on('value', (snapshot) => {
+            snapshot.forEach((childSnap) => {
+                this.setState({
+                    socials: update(this.state.socials, {[childSnap.key]: {$set: childSnap.val()}})
+                });
+            } );
+        } );
+    }
+
+    listSocialItems() {
+        return Object.entries(this.state.socials).map( social => {
+            return <li key={social[0]} className="social__link">
+                        {social[1].caption}, {social[1].link}, {social[1].icon}
+                   </li>
+        });
+    }
+
+    handleAddSocial = (evt) => {
+        this.db.child('socials').push({
+            sort: 1,
+            caption: 'new social connection',
+            link: 'please set your connection link',
+            icon: 'set link to icon to dislay'
+        });
+    }
+
     render() {
         return (
             <div className="socials-view limit-width">
-                <h2>Socials placeholder</h2>
+                <ul className="socials__list">
+                  { this.listSocialItems() }
+                  <li className="socials__add" onClick={this.handleAddSocial}>+</li>
+                </ul>
             </div>
         );
     }
